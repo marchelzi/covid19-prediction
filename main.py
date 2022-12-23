5a45c5c581c814b6a6bb38db0b5fd542fb531d93
Marchel Maulana Fahrezi <marchel@sti-group.co.id>
2022-12-24 01:32:44 +0700

    add streamlit sidebar

--- main.py
+++ main.py
@@ -26,23 +26,27 @@ def process_input(input_features):
 
 
 def build_input():
-    usmer = st.selectbox("Medical History", ("Yes", "No"))
-    sex = st.selectbox("What is your gender?", ("Female", "Male"))
-    patient_type = st.selectbox(
-        "What is your patient type?", ("Returned Home", "Hospitalized"))
-    pneumonia = st.selectbox("Do you have Pneumonia?", ("Yes", "No"))
-    age = st.select_slider("What is your Age?", options=range(0, 100))
-    pregnant = st.selectbox("Are you pregnant?", ("Yes", "No"))
-    diabetes = st.selectbox("Do you have Diabetes?", ("Yes", "No"))
-    copd = st.selectbox("Do you have COPD?", ("Yes", "No"))
-    ashma = st.selectbox("Do you have Asthma?", ("Yes", "No"))
-    insupr = st.selectbox("Do you have Inmsupr?", ("Yes", "No"))
-    hipertension = st.selectbox("Do you have Hipertension?", ("Yes", "No"))
-    other_disease = st.selectbox("Do you have Other Disease?", ("Yes", "No"))
-    cardiovascular = st.selectbox("Do you have Cardiovascular?", ("Yes", "No"))
-    obesity = st.selectbox("Do you have Obesity?", ("Yes", "No"))
-    renal_chronic = st.selectbox("Do you have Renal Chronic?", ("Yes", "No"))
-    tobacco = st.selectbox("Do you have Tobacco?", ("Yes", "No"))
+    with st.sidebar:
+        usmer = st.selectbox("Medical History", ("Yes", "No"))
+        sex = st.selectbox("What is your gender?", ("Female", "Male"))
+        patient_type = st.selectbox(
+            "What is your patient type?", ("Returned Home", "Hospitalized"))
+        pneumonia = st.selectbox("Do you have Pneumonia?", ("Yes", "No"))
+        age = st.select_slider("What is your Age?", options=range(0, 100))
+        pregnant = st.selectbox("Are you pregnant?", ("Yes", "No"))
+        diabetes = st.selectbox("Do you have Diabetes?", ("Yes", "No"))
+        copd = st.selectbox("Do you have COPD?", ("Yes", "No"))
+        ashma = st.selectbox("Do you have Asthma?", ("Yes", "No"))
+        insupr = st.selectbox("Do you have Inmsupr?", ("Yes", "No"))
+        hipertension = st.selectbox("Do you have Hipertension?", ("Yes", "No"))
+        other_disease = st.selectbox(
+            "Do you have Other Disease?", ("Yes", "No"))
+        cardiovascular = st.selectbox(
+            "Do you have Cardiovascular?", ("Yes", "No"))
+        obesity = st.selectbox("Do you have Obesity?", ("Yes", "No"))
+        renal_chronic = st.selectbox(
+            "Do you have Renal Chronic?", ("Yes", "No"))
+        tobacco = st.selectbox("Do you have Tobacco?", ("Yes", "No"))
 
     input_features = [
         usmer,
@@ -66,25 +70,32 @@ def build_input():
     return process_input(input_features)
 
 
-model = load_model("random_forest.pkl")
+with st.spinner("Loading Model Into Memory..."):
+    model = load_model("random_forest.pkl")
 
 
 def main():
 
-    st.title("Covid-19 Prediction")
-    st.write("Please fill the following information to predict the Covid 19")
-    st.write("## Input Features")
+    st.title("Covid-19 Death Prediction")
+
+    with st.sidebar:
+        st.write("Please fill the following information to predict the Covid 19")
+        st.write("## Input Features")
 
     input_data = build_input()
 
-    prediction = model.predict_proba([input_data])
-
-    if prediction[0][0] > 0.5:
-        prediction = "Negative"
-    else:
-        prediction = "Positive"
-
-    st.write("High Risk of Covid-19: ", prediction)
+    with st.spinner("Predicting..."):
+        prediction = model.predict_proba([input_data])
+        if prediction[0][0] > 0.5:
+            message = "### High Risk of Covid-19 Death: No" + \
+                " (Accuracy: " + str(prediction[0][0]) + "~)"
+            st.success(message)
+            st.write("### Please stay safe!")
+        else:
+            message = "### High Risk of Covid-19 Death: Yes" + \
+                " (Accuracy: " + str(prediction[0][1]) + "~)"
+            st.error(message)
+            st.write("### Please contact your doctor immediately, and stay safe!")
 
 
 if __name__ == "__main__":
